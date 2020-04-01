758a960bc342740f95efcecf2b9637360b1ee4a8
harut0011 <metsatunyanharut7@gmail.com>
2020-04-01 14:07:02 +0300

    1.04

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # appear-js
 Appearing effect by harut0011
 
-1) Connect the link to your site
+1) Connect the link to your html document
 
-2) 
+2) Enter the animation name as class for element
+
+3) Enter "offset = *enter here the height of vision you want to animate after*"(offset is an already declared variable) in the first line of YOUR .js file. For example: offset = 350
--- appear.js
+++ appear.js
@@ -1,1 +1,18 @@
-alert('yo')
+alert('appear.js connected')
+let offset // You gotta assign a value to this in your js document
+let startPos // Start position for element
+let finishPos // Finish poxition for element
+function anim(animName, animClassOn, animClass) {
+	animName.forEach(function(element, index) {
+		if ((window.innerHeight - element.getBoundingClientRect().top) > offset) {
+			element.classList.add(animClassOn);
+			// element.classList.remove(animClass)
+		}
+	})
+}
+
+
+const fadeUps = document.querySelectorAll('.fade-up');
+window.addEventListener('scroll', () => {
+	anim(fadeUps, 'fade-up-on')
+})
